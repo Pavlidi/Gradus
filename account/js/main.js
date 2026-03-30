48e02f5bbe15008af067929cf8664f0b2e4d7db9
Sergey <pavlidi.s.u@gmail.com>
2026-03-30 16:44:31 +0700

    MSC

--- account/js/main.js
+++ account/js/main.js
@@ -32,3 +32,56 @@ subjects.forEach(subject => {
 
     });
 });
+
+function openTasks()
+{
+    var subjectSwiper = document.getElementsByClassName('subject__swiper');
+    for (let i = 0; i < subjectSwiper.length; i++) {
+        subjectSwiper[i].style.display = "block";
+    }
+    
+    var sectionTask = document.getElementsByClassName('section-task');
+    for (let i = 0; i < sectionTask.length; i++) {
+        sectionTask[i].style.display = "block";
+    }
+
+}
+
+function openStatistics()
+{
+    var subjectSwiper = document.getElementsByClassName('subject__swiper');
+    for (let i = 0; i < subjectSwiper.length; i++) {
+        subjectSwiper[i].style.display = "block";
+    }
+
+    var sectionTask = document.getElementsByClassName('section-task');
+    for (let i = 0; i < sectionTask.length; i++) {
+        sectionTask[i].style.display = "none";
+    }
+}
+
+function openMaterials()
+{
+    var subjectSwiper = document.getElementsByClassName('subject__swiper');
+    for (let i = 0; i < subjectSwiper.length; i++) {
+        subjectSwiper[i].style.display = "none";
+    }
+    
+    var sectionTask = document.getElementsByClassName('section-task');
+    for (let i = 0; i < sectionTask.length; i++) {
+        sectionTask[i].style.display = "none";
+    }
+}
+
+function openProfile()
+{
+    var subjectSwiper = document.getElementsByClassName('subject__swiper');
+    for (let i = 0; i < subjectSwiper.length; i++) {
+        subjectSwiper[i].style.display = "none";
+    }
+
+    var sectionTask = document.getElementsByClassName('section-task');
+    for (let i = 0; i < sectionTask.length; i++) {
+        sectionTask[i].style.display = "none";
+    }
+}
